801add1c61c603f86e5f9d0f45bbb45f3e089afc
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-05 19:58:46 +0300

    allow cors

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
